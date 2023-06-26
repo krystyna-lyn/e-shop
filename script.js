37f4e965fd464f9082e6b37014b6c6478d3d909a
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-06-26 23:58:06 +0100

    update product page

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
+const mainImg = document.getElementById('mainImg');
+const smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function () {
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function () {
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function () {
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function () {
+    mainImg.src = smallImg[3].src;
+}
+
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
