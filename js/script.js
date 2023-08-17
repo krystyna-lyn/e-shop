7f8da252801301e476ea9558d49174a17590da56
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-08-17 21:01:35 +0100

    update cart

--- js/script.js
+++ js/script.js
@@ -24,8 +24,6 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
-
-
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
