7e058435c4c7bc19db4d3b5b86985252110358c0
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-08-23 11:47:02 +0100

    update navbar

--- js/essentials.js
+++ js/essentials.js
@@ -1,51 +1,51 @@
 const tshirts = [
   {
-      id: "1",
-      productName: `T-Shirt white vogue`,
-      image: "img/pr1.jpg",
-      productPrice: 100
+    id: "1",
+    productName: `T-Shirt white vogue`,
+    image: "img/pr1.jpg",
+    productPrice: 100
   },
   {
-      id: "2",
-      productName: `T-Shirt black`,
-      image: "img/pr2.jpg",
-      productPrice: 90
+    id: "2",
+    productName: `T-Shirt black`,
+    image: "img/pr2.jpg",
+    productPrice: 90
   },
   {
-      id: "3",
-      productName: `Top white`,
-      image: "img/pr3.jpg",
-      productPrice: 120
+    id: "3",
+    productName: `Top white`,
+    image: "img/pr3.jpg",
+    productPrice: 120
   },
   {
-      id: "4",
-      productName: `T-Shirt white`,
-      image: "img/pr4.jpg",
-      productPrice: 150
+    id: "4",
+    productName: `T-Shirt white`,
+    image: "img/pr4.jpg",
+    productPrice: 150
   },
   {
-      id: "5",
-      productName: `T-Shirt whatever`,
-      image: "img/pr5.jpg",
-      productPrice: 100
+    id: "5",
+    productName: `T-Shirt whatever`,
+    image: "img/pr5.jpg",
+    productPrice: 100
   },
   {
-      id: "6",
-      productName: `T-Shirt animals`,
-      image: "img/pr6.jpg",
-      productPrice: 65
+    id: "6",
+    productName: `T-Shirt animals`,
+    image: "img/pr6.jpg",
+    productPrice: 65
   },
   {
-      id: "7",
-      productName: `T-Shirt white`,
-      image: "img/pr7.jpg",
-      productPrice: 70
+    id: "7",
+    productName: `T-Shirt white`,
+    image: "img/pr7.jpg",
+    productPrice: 70
   },
   {
-      id: "8",
-      productName: `T-Shirt white transparent`,
-      image: "img/pr8.jpg",
-      productPrice: 80
+    id: "8",
+    productName: `T-Shirt white transparent`,
+    image: "img/pr8.jpg",
+    productPrice: 80
   },
 ]
 
@@ -119,7 +119,7 @@ document.getElementById("header").innerHTML =
 
     <div id="mobile">
      
-      <i id="bar" class="fas fa-outdent"></i>
+      <i id="bar" class="fas fa-bars"></i>
     </div>
 
     `;
