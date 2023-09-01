614ab6e6577e3c87b3d1bb8bf0f489ecdf9caa8b
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-09-01 20:26:23 +0100

    update product

--- js/products.js
+++ js/products.js
@@ -1,6 +1,6 @@
 // show products
 let container = document.getElementsByClassName("pro-container")[0];
-let pDetails = document.getElementById("pDetails");
+
 
 tshirts.forEach((element) => {
     const { id, productName, image, productPrice } = element;
@@ -31,30 +31,7 @@ tshirts.forEach((element) => {
     container.appendChild(div);
 });
 
-pDetails.innerHTML= `
 
-<h2 id="${productName}"></h2>
-<p>Womans's Fashion T-Shirt</p>
-<h2 id="productPrice">${productPrice}</h2>
-<select>
-  <option>Select size</option>
-  <option>XSmall</option>
-  <option>Small</option>
-  <option>Medium</option>
-  <option>Large</option>
-</select>
-<input type="number" value="${id}" />
-<button  class="btn" data-product-id="">Add to Cart</button>
 
-<h4>Product Details</h4>
-<span
-  >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-  cumque impedit fugiat voluptates facilis dicta ut incidunt aspernatur
-  Voluptas libero deserunt doloribus voluptatem eum eius quae obcaecati
-  cupiditate vel similique Lorem ipsum dolor sit amet consectetur
-  adipisicing elit. Aliquid cumque impedit fugiat voluptates facilis
-  dicta ut incidunt aspernatur Voluptas libero deserunt doloribus
-  voluptatem eum eius quae obcaecati cupiditate vel similique
-</span>
 
-`;
+
